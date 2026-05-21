d0ee5f293771e03fc17e41bc26fe55c82a887447
Carson Rivera <carsonrivera04@gmail.com>
2026-05-21 14:39:15 -0400

    Created frontend api to remove mapbox call from broswer (#22)

--- frontend/src/components/ActivityImg.tsx
+++ frontend/src/components/ActivityImg.tsx
@@ -1,22 +1,5 @@
 export const ActivityImg = ({ polyline }: { polyline: string }) => {
-  const STROKEWIDTH = 3;
-  const STROKECOLOR = "f44";
-  const STROKEOPACITY = 1;
-  
-  const POLYLINE_UNSAFE = polyline;
-  
-  const POLYLINE = encodeURIComponent(POLYLINE_UNSAFE);
-
-  const USERNAME = "mapbox";
-  const STYLE_ID = "streets-v12";
-  const OVERLAY = `path-${STROKEWIDTH}+${STROKECOLOR}-${STROKEOPACITY}(${POLYLINE})`;
-  const OTHER = "auto";
-  const WIDTH = 500;
-  const HEIGHT = 300;
-  
-  const ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
-
-  const mapboxImageUrl = `https://api.mapbox.com/styles/v1/${USERNAME}/${STYLE_ID}/static/${OVERLAY}/${OTHER}/${WIDTH}x${HEIGHT}?access_token=${ACCESS_TOKEN}`;
+  const mapboxImageUrl = `/api/mapbox-static?polyline=${encodeURIComponent(polyline)}`;
 
   return (
     <img
