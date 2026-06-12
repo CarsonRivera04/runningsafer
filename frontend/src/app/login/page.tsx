74f53d72e6da7f7bb120e9eb2cac62636080ba7c
Carson Rivera <carsonrivera04@gmail.com>
2026-06-11 21:03:01 -0400

    Added new header to login page (#45)

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,9 +1,11 @@
 import { Feature4 } from '@/components/ui/feature4';
+import { HeaderLogin1 } from '@/components/ui/header_login';
 
 export default async function Page() {
 
   return (
     <main>
+        <HeaderLogin1 />
         <div><Feature4/></div>
     </main>
   );
