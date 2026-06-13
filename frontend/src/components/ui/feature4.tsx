18a307918a3dc5d8eb6e1abc530b66bd6e13a12a
Carson Rivera <carsonrivera04@gmail.com>
2026-06-13 10:36:43 -0400

    Changed sample text on login (#45)

--- frontend/src/components/ui/feature4.tsx
+++ frontend/src/components/ui/feature4.tsx
@@ -7,18 +7,12 @@ export const Feature4 = () => (
     <div className="container mx-auto">
       <div className="flex flex-col lg:flex-row gap-10 lg:items-center">
         <div className="flex gap-4 flex-col flex-1">
-          <div>
-            <Badge>Platform</Badge>
-          </div>
           <div className="flex gap-2 flex-col">
             <h2 className="text-xl md:text-3xl md:text-5xl tracking-tighter lg:max-w-xl font-regular text-left">
-              This is the start of something new
+              Running Safer
             </h2>
             <p className="text-lg max-w-xl lg:max-w-sm leading-relaxed tracking-tight text-muted-foreground text-left">
-              Managing a small business today is already tough. Avoid further
-              complications by ditching outdated, tedious trade methods. Our
-              goal is to streamline SMB trade, making it easier and faster than
-              ever.
+              Connect your Strava account to Running Safer and get personalized safety insights based on your running and walking activities. 
             </p>
           </div>
           <a href="/api/py/auth/login">
