4c790dd2a1bd6c2634fecd1cb8b989cdfa62f291
Carson Rivera <carsonrivera04@gmail.com>
2026-06-13 20:28:15 -0400

    Added initial about page (#47)

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import { Header1 } from '@/components/ui/header';
+import { FAQ2 } from '@/components/ui/faq2';
+
+export default async function Page() {
+
+  return (
+    <main>
+        <Header1/>
+        <FAQ2/>
+    </main>
+  );
+}
